dafc50d3d5df8ef018c57eb175828fbfb710cf82
Unieo <unieocorporation@yahoo.com>
2021-01-27 13:44:32 +0530

    Cleaned up the code

--- commands/kick.js
+++ commands/kick.js
@@ -1,32 +1,37 @@
-const Discord = require("discord.js")
-const config = require("../config/config.json")
+const Discord = require("discord.js");
+const config = require("../config/config.json");
 
 module.exports = {
-	name: "kick",
-	description: "Kicks a mentioned user in a Guild.",
-	cooldown: 2,
-	run(client, message, args){
-		if (message.channel instanceof Discord.DMChannel) return;
-		const user = message.mentions.users.first()
-		if (user == config.botid) return message.channel.send("**Error:** You cannot kick me!")
-		if (user == message.author.id) return message.channel.send("**Error:** You cannot kick yourself!")
-		if(user){
-			const member = message.guild.members.cache.get(user.id)
-	    member.kick().then(() => {
-	    	const embed = new Discord.MessageEmbed()
-          .setTitle("Member Kicked from Server")
-          .setColor(Math.floor(Math.random() * 16777215))
-          .setDescription(
-            `**<@${user.id}> was kicked by <@${message.author.id}>.**`
-          )
-          .setTimestamp()
-          .setFooter(`${config.copyright}`);
-        message.channel.send({ embed });
-	    }).catch(err => {
-	    	console.error(err)
-	    })
-		} else {
-			message.channel.send("**Error:** Member not found!")
-		}
-	}
-}
+    name: "kick",
+    description: "Kicks a mentioned user in a Guild.",
+    cooldown: 2,
+    run(client, message, args) {
+        if (message.channel instanceof Discord.DMChannel) return;
+        const user = message.mentions.users.first();
+        if (user == config.botid)
+            return message.channel.send("**Error:** You cannot kick me!");
+        if (user == message.author.id)
+            return message.channel.send("**Error:** You cannot kick yourself!");
+        if (user) {
+            const member = message.guild.members.cache.get(user.id);
+            member
+                .kick()
+                .then(() => {
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle("Member Kicked from Server")
+                        .setColor(Math.floor(Math.random() * 16777215))
+                        .setDescription(
+                            `**<@${user.id}> was kicked by <@${message.author.id}>.**`
+                        )
+                        .setTimestamp()
+                        .setFooter(`${config.copyright}`);
+                    message.channel.send({ embed });
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        } else {
+            message.channel.send("**Error:** Member not found!");
+        }
+    },
+};
--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const config = require("../config/config.json");
 
 module.exports = {
     name: "ping",
-		description: "Sends the bot and api latency",
-		cooldown: 2,
+    description: "Sends the bot and api latency",
+    cooldown: 2,
     run(client, message, args) {
         let latency = Date.now() - message.createdTimestamp;
         let api = Math.round(client.ws.ping);
